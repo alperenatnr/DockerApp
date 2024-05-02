fc7851df6fe749af888314dcc77615033adc2d3e
Mehmet Alperen ATANER <alperenataner10@hotmail.com>
2024-05-02 15:47:29 +0300

    Add files via upload

---
+++ src/main/java/Controller/MainController.java
@@ -0,0 +1,21 @@
+package Controller;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author alper
+ */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index"; // index.html'yi döndürmek için controller metodu
+    }
+}
